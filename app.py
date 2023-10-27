05f02f137e9717fa6ddebb938d294ba18133599a
Mohan <111583061+mohain9@users.noreply.github.com>
2023-10-27 09:26:33 +0200

    app.py

--- app.py
+++ app.py
@@ -4,10 +4,9 @@ from PIL import Image
 
 
 #path settings                      
-current_dir= Path(__file__).parent if "__file__" in locals() else Path.cwd()
-css_file= current_dir/"styles"/"main.css"
-resume_file= current_dir/"assets"/"cv.pdf"
-profile_pic= current_dir/"assets"/"profile-pic.png"
+#css_file= current_dir/"styles"/"main.css"
+resume_file= "cv.pdf"
+profile_pic= "profile-pic.png"
 
 #general settings
 
